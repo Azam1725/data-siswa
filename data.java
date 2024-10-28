8b3c356de5b5818a149eefa2796dfe372f558524
Azam1725 <moch._mahardika_33rpl@student.smktelkom-mlg.sch.id>
2024-10-28 11:32:58 +0700

    Add files via upload

---
+++ data.java
@@ -0,0 +1,17 @@
+public class data {
+    public static void main(String[] args) {
+        //string = kata, kalimat, paragraf
+        String nama = "Azam";
+        //int = angka bulat
+        int usia = 16;
+        String alamat = "JL.Jaya Srani 1 7K/21";
+        //double = angka pecahan atau desimal
+        double kelas = 10.7;
+
+        System.out.println("Nama saya " + nama);
+        System.out.println("berusia " + usia + " tahun");
+        System.out.println("tinggal di " + alamat);
+        System.out.println("Kelas " +kelas);
+    }
+
+}
